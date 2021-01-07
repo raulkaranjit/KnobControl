9905938a3cb626c92561c5cea0e5f91bee2d605b
Aashish Karn <aashish.karn@cellcom.net.np>
2021-01-07 16:11:04 +0545

    Initial Commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Cellcom on 1/7/21.
+//  Copyright © 2021 CellPay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
